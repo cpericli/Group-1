f0fcb75a47191815d43819edfdf6658cc56b73a5
HersieG <hersiedgonzalez@gmail.com>
2025-11-07 17:24:33 -0500

    Refactor: changed menu to menuItems

--- api/models/menuItems.py
+++ api/models/menuItems.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import relationship
 from datetime import datetime
 from ..dependencies.database import Base
 
-class Menu(Base):
+class MenuItems(Base):
     __tablename__ = "menu_items"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
