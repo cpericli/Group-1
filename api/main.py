9638cb2f33f6cb7adb5d83495186132545dc9ebd
Nora Miller <nmille73@charlotte.edu>
2025-11-18 10:45:13 -0500

    Edited API tag to make it more readable

--- api/main.py
+++ api/main.py
@@ -28,22 +28,22 @@ indexRoute.load_routes(app)
 if __name__ == "__main__":
     uvicorn.run(app, host=conf.app_host, port=conf.app_port)
 
-@app.post("/ratings_and_reviews/", response_model=model_loader.Rating, tags=["RatingsAndReviews"])
+@app.post("/ratings_and_reviews/", response_model=model_loader.Rating, tags=["Ratings And Reviews"])
 def create_ratings_and_reviews(rating: model_loader.RatingCreate, db: Session= Depends(get_db)):
     return create_ratings_and_reviews.create(db=db, rating=rating)
 
-@app.get("/ratings_and_reviews/", response_model=List[model_loader.Rating], tags=["RatingsAndReviews"])
+@app.get("/ratings_and_reviews/", response_model=List[model_loader.Rating], tags=["Ratings And Reviews"])
 def read_all_ratings(db: Session= Depends(get_db)):
     return ratingsAndReviews.read_all(db=db)
 
-@app.put("/ratings_and_reviews/{rating_id}", response_model=model_loader.Rating, tags=["RatingsAndReviews"])
+@app.put("/ratings_and_reviews/{rating_id}", response_model=model_loader.Rating, tags=["Ratings And Reviews"])
 def update_one_rating_and_review(rating_id: int, rating: model_loader.RatingUpdate, db: Session = Depends(get_db)):
     rating_db = ratingsAndReviews.read_one(db=db, rating_id=rating_id)
     if rating_db is None:
         raise HTTPException(status_code=404, detail="Rating not found")
     return ratingsAndReviews.update(db=db, rating_id=rating_id, rating=rating)
 
-@app.delete("/ratings_and_reviews/{rating_id}", response_model=model_loader.Rating, tags=["RatingsAndReviews"])
+@app.delete("/ratings_and_reviews/{rating_id}", response_model=model_loader.Rating, tags=["Ratings And Reviews"])
 def delete_rating(rating_id: int, db: Session = Depends(get_db)):
     rating: ratingsAndReviews.read_one(db=db, rating_id=rating_id)
     if rating is None:
