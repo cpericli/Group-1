a278ad05878464d4ab41e3ba39a9b95d5065c6bc
Chris_Pericli <cpericli@charlotte.edu>
2025-11-22 16:30:19 -0500

    update orders schemas

--- api/schemas/orders.py
+++ api/schemas/orders.py
@@ -4,34 +4,63 @@ from pydantic import BaseModel, Field
 from decimal import Decimal
 from .order_details import OrderDetail
 
-class OrderBase(BaseModel):
+
+class OrderCreate(BaseModel):
     description: Optional[str] = Field(
         default=None,
         title="Delivery / Takeout / Dine-In",
         description="Specify how you'd like to receive your order (e.g. 'Delivery', 'Takeout', or 'Dine-In')."
     )
-    order_status: Optional[str] = None
-
-class OrderCreate(OrderBase):
     customer_id: int
+    promotion_id: Optional[int] = None
+
 
 class OrderUpdate(BaseModel):
     description: Optional[str] = None
     order_status: Optional[str] = None
     promotion_id: Optional[int] = None
 
-class Order(OrderBase):
+
+class Order(BaseModel):
     id: int
     order_date: Optional[datetime] = None
+    order_type: Optional[str] = Field(
+        default=None,
+        alias="description",
+        title="Dine-in / Takeout / Delivery",
+        description="How the order will be received."
+    )
+    order_status: str
     customer_id: int
     promotion_id: Optional[int] = None
     order_details: list[OrderDetail] = None
 
     class ConfigDict:
         from_attributes = True
+        populate_by_name = True
+
+
+class OrderPublic(BaseModel):
+    id: int
+    order_date: Optional[datetime] = None
+
+    order_type: Optional[str] = Field(
+        default=None,
+        alias="description",
+        title="Dine-in / Takeout / Delivery",
+        description="How the order will be received."
+    )
+
+    customer_id: int
+    promotion_id: Optional[int] = None
+    order_details: list[OrderDetail] = None
+
+    class ConfigDict:
+        from_attributes = True
+        populate_by_name = True
+
 
 class OrderWithPricing(Order):
-    """Extended order schema with pricing information"""
     total_price: Decimal
     discounted_total: Decimal
     discount_applied: Optional[Decimal] = None
