86f8734cf1dc88635eaa6383f713e015bfcbe93e
Nora Miller <nmille73@charlotte.edu>
2025-11-25 10:13:15 -0500

    changed mySQL password

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -3,6 +3,6 @@ class conf:
     db_name = "sandwich_maker_api_final"
     db_port = 3306
     db_user = "root"
-    db_password = "blueblue"
+    db_password = "T@nger!ne87"
     app_host = "localhost"
     app_port = 8000
